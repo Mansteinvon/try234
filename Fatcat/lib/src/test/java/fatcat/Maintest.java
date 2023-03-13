a097721cf36fb431b936d78f356f71b748e944d2
Mansteinvon <36380930+Mansteinvon@users.noreply.github.com>
2023-03-13 07:01:23 -0700

    test case added

---
+++ Fatcat/lib/src/test/java/fatcat/Maintest.java
@@ -0,0 +1,14 @@
+package fatcat;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Maintest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
